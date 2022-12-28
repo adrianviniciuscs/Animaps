5422d0aa20b8bca4c1d568d03a43cea17766e004
Adrian Vinicius <93016748+adrianviniciuscs@users.noreply.github.com>
2022-12-28 16:25:31 -0300

    Feature de adicionar marcador com click

--- animap/animais/static/animais/js/app.js
+++ animap/animais/static/animais/js/app.js
@@ -1,3 +1,15 @@
+// map
+
+var mymap = L.map('mapid').setView([-6.1124964, -38.2062628], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(mymap);
+
+
+
+
 var endereçoLocal = document.querySelector('#localização');
 
 var endereços = document.querySelector('#endereços');
@@ -5,6 +17,33 @@ var endereços = document.querySelector('#endereços');
 var endereço = document.querySelector('#id_endereço');
 var coordX = document.querySelector('#id_coordX');
 var coordY = document.querySelector('#id_coordY');
+
+var marker = null
+var clickendr = ""
+
+
+mymap.on('click', function(e) {
+
+    if (marker) {
+        mymap.removeLayer(marker);
+      }
+    var lat = e.latlng.lat;
+    var lng = e.latlng.lng;
+    url = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lng}`;
+    
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            endereço.value = data.display_name; 
+        });
+        
+
+    console.log(clickendr) 
+    coordX.value = lat
+    coordY.value = lng
+
+    marker = L.marker([lat, lng]).addTo(mymap);
+});
 
 function mostrarEndereços(){
     endereços.innerHTML = ""
@@ -33,13 +72,3 @@ function acheEndereço(){
             .then(data => addressData = data)
             .then(mostrarEndereço => mostrarEndereços())
 };
-
-
-// map
-
-var mymap = L.map('mapid').setView([-6.1124964, -38.2062628], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(mymap);
